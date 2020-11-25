5ecdf8f073c2fc45de53577b5d139b5c6d89d9de
voytasic <arekw37@gmail.com>
2020-11-25 04:23:10 +0100

    Remove main() implementation

--- src/com/voytasic/Blog.java
+++ src/com/voytasic/Blog.java
@@ -32,15 +32,5 @@ public class Blog implements Observable {
         notifyObservers();
     }
 
-    public static void main(String[] args) {
-        Blog samouczek = new Blog();
-        Reader reader1 = new Reader(samouczek);
-        Reader reader2 = new Reader(samouczek);
-        samouczek.attach(reader1);
-        samouczek.attach(reader2);
-        samouczek.publishArticle("Killer article title!");
 
-        samouczek.detach(reader2); // sad panda :(
-        samouczek.publishArticle("Another killer article title!");
-    }
 }
