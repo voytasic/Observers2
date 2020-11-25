a5558f11807cd608430832b54363067f5bfe1514
voytasic <arekw37@gmail.com>
2020-11-25 04:22:36 +0100

    Add objects to main() method

--- src/com/voytasic/Main.java
+++ src/com/voytasic/Main.java
@@ -3,6 +3,14 @@ package com.voytasic;
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+        Blog samouczek = new Blog();
+        Reader reader1 = new Reader(samouczek);
+        Reader reader2 = new Reader(samouczek);
+        samouczek.attach(reader1);
+        samouczek.attach(reader2);
+        samouczek.publishArticle("Killer article title!");
+
+        samouczek.detach(reader2); // sad panda :(
+        samouczek.publishArticle("Another killer article title!");
     }
 }
