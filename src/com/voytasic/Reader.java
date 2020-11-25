f0abf9a62631fd577f9b88b748736952b4da847b
voytasic <arekw37@gmail.com>
2020-11-25 04:32:57 +0100

    Add subscribe() & unsubscribe()

--- src/com/voytasic/Reader.java
+++ src/com/voytasic/Reader.java
@@ -1,10 +1,12 @@
 package com.voytasic;
 
 public class Reader implements Observer {
+    private String readerName;
     private final Blog blog;
     private String newestArticle;
 
-    public Reader(Blog blog) {
+    public Reader(String readerName, Blog blog) {
+        this.readerName = readerName;
         this.blog = blog;
         newestArticle = blog.getNewestArticle();
     }
@@ -13,5 +15,11 @@ public class Reader implements Observer {
     public void update() {
         newestArticle = blog.getNewestArticle();
         System.out.println(String.format("An article „%s” was published!",  newestArticle));
+    }
+    public void subscribe() {
+        this.blog.attach(this);
+    }
+    public void unsubscribe(){
+        this.blog.detach(this);
     }
 }
